4798167a4d577e3f49109e88ba18f5986209c26f
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2025-02-06 21:43:06 -0500

    calculator works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let weight = calculate_weight_on_mars(100.0);
+    println!("Your weight in Mars is {} Kg", weight);
+}
+
+fn calculate_weight_on_mars(w: f32) -> f32 {
+    (w / 9.81) * 3.711
 }
