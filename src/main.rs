685ca67a2033a932bbabc86ddbc52e9e165b725a
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2025-02-07 20:45:01 -0500

    reading user input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use std::io;
 fn main() {
-    let weight = calculate_weight_on_mars(100.0);
-    println!("Your weight in Mars is {} Kg", weight);
+    //print message to terminal
+    println!("Enter your weight in Earth");
+    //create new string variable called input
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input) //reads the input from user
+        .expect("Failed to read line"); //expect() handles any errors
+
+    let weight: f32 = input.trim().parse().expect("Please type a number!"); //trim() removes whitespace, parse()converts string to f32
+
+    let mut mars_weight = calculate_weight_on_mars(weight);
+
+    mars_weight = mars_weight * 1000.0;
+
+    println!("Your weight in Mars is {} gr", mars_weight);
 }
 
 fn calculate_weight_on_mars(w: f32) -> f32 {
