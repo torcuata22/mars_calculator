751c8e4d1f2b8d8ded8e7afb333450868b8c6a2a
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2025-02-07 21:19:28 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::io;
 fn main() {
     //print message to terminal
-    println!("Enter your weight in Earth");
+    println!("Enter your Terran weight in kg");
     //create new string variable called input
     let mut input = String::new();
 
     io::stdin()
         .read_line(&mut input) //reads the input from user
-        .expect("Failed to read line"); //expect() handles any errors
+        .unwrap(); //terminates program in case of error from readline
+                   //.expect("Failed to read line"); //expect() handles any errors
 
-    let weight: f32 = input.trim().parse().expect("Please type a number!"); //trim() removes whitespace, parse()converts string to f32
+    let weight: f32 = input.trim().parse().unwrap(); //trim() removes whitespace, parse()converts string to f32
 
     let mut mars_weight = calculate_weight_on_mars(weight);
 
